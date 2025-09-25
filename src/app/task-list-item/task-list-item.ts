a33ed06b7b20e7521e23b4446c8abaa5bcfef0fc
abdul1907 <your-github-email@example.com>
2025-09-25 17:43:24 -0400

    Created 2 Components

---
+++ src/app/task-list-item/task-list-item.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-list-item',
+  imports: [],
+  templateUrl: './task-list-item.html',
+  styleUrl: './task-list-item.css'
+})
+export class TaskListItem {
+
+}
