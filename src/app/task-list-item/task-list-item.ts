405df37e140b4f6d9d69ac268cef3c482fdcb415
abdul1907 <your-github-email@example.com>
2025-10-02 16:47:14 -0400

    Complete steps 6-8, define array, add item tags, add @input  property

--- src/app/task-list-item/task-list-item.ts
+++ src/app/task-list-item/task-list-item.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TideNode } from '../Models/tidenode.interface';
+
 
 @Component({
   selector: 'app-task-list-item',
+  standalone: true,
   imports: [],
   templateUrl: './task-list-item.html',
   styleUrl: './task-list-item.css'
 })
 export class TaskListItem {
-
+  @Input() task!: TideNode;
 }
