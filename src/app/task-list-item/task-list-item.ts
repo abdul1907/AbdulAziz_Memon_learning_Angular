6e983d5ed4085d90847d626e36d74725370474e9
abdul1907 <your-github-email@example.com>
2025-10-03 10:39:08 -0400

    Update the TaskListItem component to displays in the way asked

--- src/app/task-list-item/task-list-item.ts
+++ src/app/task-list-item/task-list-item.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { TideNode } from '../Models/tidenode.interface';
 
 
 @Component({
   selector: 'app-task-list-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './task-list-item.html',
   styleUrl: './task-list-item.css'
 })
