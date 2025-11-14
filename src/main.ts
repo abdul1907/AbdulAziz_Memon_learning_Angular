1ed3adb69d38aff5f3c1c194a62a541dc7cda2ff
abdul1907 <your-github-email@example.com>
2025-11-13 22:33:25 -0500

    Add Lazy loading to routes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';  //bootstrapApplication is a function that bootstraps the application
 import { provideRouter } from  '@angular/router';  //provideRouter is a function that provides the router configuration
-import { Routes } from '@angular/router';  //Routes is a type that defines the routes of the application
+import { Routes} from '@angular/router';  //Routes is a type that defines the routes of the application
 import { AppComponent } from './app/app';
 import { TaskList } from './app/task-list/task-list';
-import { TaskListItem } from './app/task-list-item/task-list-item';
-import { ModifyListItem } from './app/modify-list-item/modify-list-item';
-import { PageNotFound } from './app/page-not-found/page-not-found';
 import { InMemoryDataService } from './app/services/in-memory-data-service';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';  
@@ -26,22 +23,22 @@ const routes: Routes = [
   //Task Detail Route with dynamic parameter
   {
     path: 'task-detail/:id',
-    component: TaskListItem
+    loadComponent: () => import('./app/task-list-item/task-list-item').then(m => m.TaskListItem)
   },
   //Create Route without id (opens empty form to add new task)
   {
     path: 'modify-list-item',
-    component: ModifyListItem
+    loadComponent: () => import('./app/modify-list-item/modify-list-item').then(m => m.ModifyListItem)
   },
   //Modify List Item Route with dynamic parameter
   {
     path: 'modify-list-item/:id',
-    component: ModifyListItem
+    loadComponent: () => import('./app/modify-list-item/modify-list-item').then(m => m.ModifyListItem)
   },
   //Wildcard route for 404 page - must be last
   {
     path: '**',
-    component: PageNotFound
+    loadComponent: () => import('./app/page-not-found/page-not-found').then(m => m.PageNotFound)
   }
 
 ];
